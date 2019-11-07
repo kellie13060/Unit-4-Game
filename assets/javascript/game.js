c8d60672c9e991770c21ddcc883789b2ae83c744
Kellie Hunsaker <kellie13060@gmail.com>
2019-11-06 23:09:57 -0700

    Added some click events and more style changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,37 @@
-var imageCoin = $("imgs");
-var imageShell = $("imgs");
-var imageWood = $("imgs");
-var imagePink = $("imgs");
+//establish the random number given to get
+//number options given and randomly chose
+
+
+//set the numberGiven to match the target number
+
+//check to see if the numbers match or have gone over
+
+//if the numbers match log the win
+//else if the number has gone over log a lose
+//else the number is too low do nothing
+
+
+
+var counter = 0;
+
+var randomNumber = Math.floor(Math.random() * (120-19) + 19);
+console.log(randomNumber);
+$("#numberGiven").append(randomNumber);
+
+
+$("#imgShell").on("click", function() {
+    alert("Clicked");
+    
+});
+$("#imgWood").on("click", function() {
+    alert("Clicked");
+    
+});
+$("#imgCoin").on("click", function() {
+    alert("Clicked");
+    
+});
+$("#imgPink").on("click", function() {
+    alert("Clicked");
+    
+});
